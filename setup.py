dbde35eaf6012362e718ab38cfa7dabc98a893eb
iYwaN82 <il7060@gmail.com>
2022-12-27 17:25:41 +0300

    17:25 27-12-22

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mlkConv',
+    version='0.18',
+    packages=[''],
+    url='',
+    license='',
+    author='iYwaN',
+    author_email='',
+    description=''
+)
